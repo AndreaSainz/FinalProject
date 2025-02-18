2fab6cb086039228c42e64c30b17f1c10742ba1b
AndreaSainz <as3628@cam.ac.uk>
2025-02-18 17:35:15 +0000

    Dataset Pytorch

--- dataset.py
+++ dataset.py
@@ -1,28 +1,50 @@
 import os
-import pandas as pd
-from torchvision.io import read_image
-import torch
 from torch.utils.data import Dataset
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-import matplotlib.pyplot as plt
+import h5py
+import torch
+from skimage.transform import radon
+import numpy as np
 
-class SinogramsDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
-        self.img_dir = img_dir
-        self.transform = transform
-        self.target_transform = target_transform
+class LoDoPaBDataset(Dataset):
+    def __init__(self, ground_truth_dir):
+        """
+        Args:
+            ground_truth_dir (str): Directory where the ground truth HDF5 files are stored.
 
+        """
+        self.ground_truth_dir = ground_truth_dir
+        # List all files in the directory
+        self.files = [os.path.join(ground_truth_dir, f) for f in os.listdir(ground_truth_dir) if f.endswith('.h5')]
+        
     def __len__(self):
-        return len(self.img_labels)
+        """Return the total number of samples in the dataset"""
+        return len(self.files)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        return image, label
+        """
+        Args:
+            idx (int): Index of the sample to be shown.
+        
+        Returns:
+            A sample containing the ground truth data images and the sinogram.
+        """
+        # Get the file corresponding to the index
+        file_path = self.files[idx]
+        
+        # Read the HDF5 file
+        with h5py.File(file_path, 'r') as f:
+            # For example, let's assume the ground truth data is stored in 'data' key in the HDF5 file
+            ground_truth = f['data'][:]
+        
+        # Convert to tensor
+        sample = torch.tensor(ground_truth, dtype=torch.float32)
+        
+        # Create Sinogram 
+        theta = np.linspace(0.0, 360.0, max(sample.shape), endpoint=False)
+        sinogram = radon(sample, theta=theta)
+        
+        # Convert the sinogram to PyTorch tensor
+        sinogram_tensor = torch.tensor(sinogram, dtype=torch.float32)
+
+    
+        return {'ground_truth': sample, 'sinogram': sinogram_tensor}
